045ea4c5bd50ba2599e621aadf2824fbafbe5bf8
Chen <fangjian_chen@jbcc.co.jp>
2019-08-09 16:33:13 +0900

    fix chart height

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -3,11 +3,6 @@ import { Bar, mixins } from 'vue-chartjs'
 export default {
   extends: Bar,
   mixins: [mixins.reactiveProp],
-  template: `
-    <div style="position: relative; margin: auto; height: 80vh; width: 80vw;">
-      <canvas id="chart"></canvas>
-    </div>
-  `,
   props: ['options'],
   mounted() {
     this.renderChart(this.chartData, this.options)
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueChart from './components/VueChart'
+import BarChart from './components/BarChart'
 import OptionPanel from './components/OptionPanel'
 import Chart from 'chart.js'
 import { mapGetters } from 'vuex'
@@ -21,11 +21,15 @@ try {
 
 new Vue({
   store,
-  components: { VueChart, OptionPanel },
+  components: { BarChart, OptionPanel },
   template: `
   <div>
     <OptionPanel />
-    <VueChart :chartData="renderData" :options="options" />
+    <BarChart
+      style="position: relative; margin: auto; height: 80vh; width: 80vw;"
+      :height="200"
+      :chartData="renderData"
+      :options="options" />
   </div>
   `,
   data(){
